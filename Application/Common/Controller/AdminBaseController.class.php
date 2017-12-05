f5a0ac0a8a8590c08d1893cc83ff34365fa9ed54
Liarman <287288189@qq.com>
2017-12-05 17:31:03 +0800

    修改session

--- Application/Common/Controller/AdminBaseController.class.php
+++ Application/Common/Controller/AdminBaseController.class.php
@@ -9,18 +9,33 @@ class AdminBaseController extends BaseController{
 	 * 初始化方法
 	 */
 	public function _initialize(){
-		//print_r(session('user'));die;
 		parent::_initialize();
 		if(empty(session('user'))){
-		/*if(session('user')==null){*/
-			$this->redirect('/Admin/Login/index');
-		}
-		$auth=new \Think\Auth();
-		$rule_name=MODULE_NAME.'/'.CONTROLLER_NAME.'/'.ACTION_NAME;
-		$result=$auth->check($rule_name,$_SESSION['user']['id']);
-		//print_r($rule_name);die;
-		if(!$result){
-			$this->error('您没有权限访问',U('Admin/Login/index'));
+			if (IS_AJAX) {
+				$result["message"]='您需要重新登录！';
+				$result["status"]=401;
+				$result["total"]=0;
+				$result["rows"] = array();
+				$this->ajaxReturn($result,'JSON');
+			}else{
+				$this->redirect('/Admin/Login/index');
+			}
+		}else {
+			$auth=new \Think\Auth();
+			$rule_name=MODULE_NAME.'/'.CONTROLLER_NAME.'/'.ACTION_NAME;
+			$result=$auth->check($rule_name,$_SESSION['user']['id']);
+			//print_r($rule_name);die;
+			if(!$result){
+				if (IS_APP || IS_AJAX) {
+					$result["message"]='您没有权限访问！';
+					$result["status"]=403;
+					$result["total"]=0;
+					$result["rows"] = array();
+					$this->ajaxReturn($result,'JSON');
+				}else{
+					$this->error('您没有权限访问',U('Admin/Login/index'));
+				}
+			}
 		}
 	}
 
